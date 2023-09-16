cad23445b6081acc3b8634c681ca7b642ecd8f40
King-Hur Wu <king2510@gmail.com>
2023-09-16 12:41:23 -0500

    complete lab

---
+++ index.js
@@ -0,0 +1,31 @@
+function shout(string) {
+    return string.toUpperCase();
+  }
+function whisper(string) {
+    return string.toLowerCase();
+}
+function logShout(string) {
+    console.log(shout(string))
+}
+function logWhisper(string) {
+    console.log(whisper(string))
+}
+function sayHiToHeadphonedRoommate(string) {
+    switch (string) {
+        case whisper(string):
+            return "I can't hear you!"
+        case shout(string):
+            return "YES INDEED!"
+            
+        case "Let's have dinner together!":
+            return "I would love to!"
+          
+    }
+    /*if (string === whisper(string)) {
+        return "I can't hear you!"
+    } elseif (string === shout(string)) {
+        return "YES INDEED!"
+    } elseif (string === "Let's have dinner together") {
+        return "I would love to!"
+    } else*/ 
+}
